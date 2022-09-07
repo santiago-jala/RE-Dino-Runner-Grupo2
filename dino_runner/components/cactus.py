03fb155d9d9123fd85dbebe328a35d6bc7860f66
santiago-jala <112905545+santiago-jala@users.noreply.github.com>
2022-09-07 00:05:35 -0400

    Creacion de obstaculo

---
+++ dino_runner/components/cactus.py
@@ -0,0 +1,9 @@
+import pygame
+from dino_runner.components.Obstaculo import Obstacle
+import random
+
+class Cactus(Obstacle):
+    def __init__ (self, image):
+        self.type = random.randint(0, 2)
+        super(). __init__ (image, self.type)
+        self.rect.y = 320
